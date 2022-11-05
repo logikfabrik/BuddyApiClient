c03ffa81351981b8d6a60ce877f6d2321000031e
logikfabrik <anton@logikfabrik.se>
2022-11-05 15:09:45 +0100

    Add unit tests

--- src/BuddyApiClient.IntegrationTest/Actions/ActionsClientTest.cs
+++ src/BuddyApiClient.IntegrationTest/Actions/ActionsClientTest.cs
@@ -1,7 +1,9 @@
 ﻿namespace BuddyApiClient.IntegrationTest.Actions
 {
+    using System.Net;
     using BuddyApiClient.Actions.Models.Response;
     using BuddyApiClient.IntegrationTest.Actions.FakeModelFactories;
+    using BuddyApiClient.IntegrationTest.Actions.Preconditions;
     using BuddyApiClient.IntegrationTest.Pipelines.Preconditions;
     using BuddyApiClient.IntegrationTest.Projects.Preconditions;
     using BuddyApiClient.IntegrationTest.Testing;
@@ -42,7 +44,46 @@
                     }
                 }
             }
+        }
 
+        public sealed class Get : BuddyClientTest
+        {
+            public Get(BuddyClientFixture fixture) : base(fixture)
+            {
+            }
+
+            [Fact]
+            public async Task Should_ReturnTheSleepAction()
+            {
+                await Preconditions
+                    .Add(new DomainExistsPrecondition(Fixture.BuddyClient.Workspaces), out var domain)
+                    .Add(new ProjectExistsPrecondition(Fixture.BuddyClient.Projects, domain), out var projectName)
+                    .Add(new PipelineExistsPrecondition(Fixture.BuddyClient.Pipelines, domain, projectName), out var pipelineId)
+                    .Add(new SleepActionExistsPrecondition(Fixture.BuddyClient.Actions, domain, projectName, pipelineId), out var actionId)
+                    .SetUp();
+
+                var sut = Fixture.BuddyClient.Actions;
+
+                var action = await sut.Get(await domain(), await projectName(), await pipelineId(), await actionId());
+
+                action.Should().BeOfType<SleepActionDetails>();
+            }
+
+            [Fact]
+            public async Task Should_Throw_When_TheSleepActionDoesNotExist()
+            {
+                await Preconditions
+                    .Add(new DomainExistsPrecondition(Fixture.BuddyClient.Workspaces), out var domain)
+                    .Add(new ProjectExistsPrecondition(Fixture.BuddyClient.Projects, domain), out var projectName)
+                    .Add(new PipelineExistsPrecondition(Fixture.BuddyClient.Pipelines, domain, projectName), out var pipelineId)
+                    .SetUp();
+
+                var sut = Fixture.BuddyClient.Actions;
+
+                var act = FluentActions.Awaiting(async () => await sut.Get(await domain(), await projectName(), await pipelineId(), ActionIdFactory.Create()));
+
+                (await act.Should().ThrowAsync<HttpRequestException>()).And.StatusCode.Should().Be(HttpStatusCode.NotFound);
+            }
         }
     }
 }
---
+++ src/BuddyApiClient.IntegrationTest/Actions/FakeModelFactories/ActionIdFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace BuddyApiClient.IntegrationTest.Actions.FakeModelFactories
+{
+    using Bogus;
+    using BuddyApiClient.Actions.Models;
+
+    internal static class ActionIdFactory
+    {
+        private static readonly Randomizer Randomizer = new();
+
+        public static ActionId Create()
+        {
+            return new ActionId(Randomizer.Int(999, 9999));
+        }
+    }
+}
